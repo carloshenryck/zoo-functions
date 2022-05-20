b63b0fd38f317da946144a3d23fca425124f6033
Henryck <ryck302@gmail.com>
2022-05-20 19:46:51 -0300

    primeiro requisito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) return [];
+
+  const species = [];
+  ids.forEach((id) => {
+    species.push(data.species.find((specie) => specie.id === id));
+  });
+
+  return species;
 }
 
 module.exports = getSpeciesByIds;
