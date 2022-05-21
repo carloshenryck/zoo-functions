776a1395315d3d804816c50bfd081823ef7b6641
Henryck <ryck302@gmail.com>
2022-05-20 21:43:13 -0300

    quarto requisito

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,23 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) => employee.managers.some((manager) => manager === id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+
+  const managed = [];
+  employees.forEach((employee) => {
+    if (employee.managers.some((manager) => manager === managerId)) {
+      managed.push(`${employee.firstName} ${employee.lastName}`);
+    }
+  });
+
+  return managed;
 }
 
 module.exports = { isManager, getRelatedEmployees };
