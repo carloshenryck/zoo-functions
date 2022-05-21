db2527a7849903165cce45b5c93c7daa0d58ac73
Henryck <ryck302@gmail.com>
2022-05-21 00:07:02 -0300

    quinto requisito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const animals = {};
+    data.species.forEach((specie) => {
+      animals[specie.name] = specie.residents.length;
+    });
+    return animals;
+  }
+
+  const equals = (a, b) => a.length === b.length && a.every((value, index) => value === b[index]);
+
+  if (equals(Object.keys(animal), ['specie'])) {
+    return data.species.find((specie) => specie.name === animal.specie).residents.length;
+  }
+
+  if (equals(Object.keys(animal), ['specie', 'sex'])) {
+    return data.species.find((specie) => specie.name === animal.specie).residents
+      .filter((resident) => resident.sex === animal.sex).length;
+  }
 }
 
 module.exports = countAnimals;
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  if (employeeName === undefined) return {};
+  if (!employeeName) return {};
 
   return data.employees.find((employee) => employee.firstName === employeeName
   || employee.lastName === employeeName);
