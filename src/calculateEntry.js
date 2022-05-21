21212c440868a317ae1670b528dc14ec3b90fe39
Henryck <ryck302@gmail.com>
2022-05-21 16:18:33 -0300

    sexto à oitavo requisito

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  return {
+    child: entrants.filter((entrant) => entrant.age < 18).length,
+    adult: entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length,
+    senior: entrants.filter((entrant) => entrant.age >= 50).length,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+  const quantity = countEntrants(entrants);
+  const { prices } = data;
+
+  return (quantity.child * prices.child) + (quantity.adult * prices.adult)
+  + (quantity.senior * prices.senior);
 }
 
 module.exports = { calculateEntry, countEntrants };
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,34 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('retorna 4 ao passar o argumento count', () => {
+    expect(handlerElephants('count')).toEqual(4);
+  });
+
+  it('retorna uma array com os nomes, ao passar o argumento names', () => {
+    const names = ['Ilana', 'Orval', 'Bea', 'Jefferson'];
+    expect(handlerElephants('names')).toEqual(names);
+  });
+
+  it('retorna a média da idade ao passar o argumento averageAge', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+
+  it('retorna null ao passar um argumento que não refere a nenhuma ação', () => {
+    expect(handlerElephants('açãoInexistente')).toBeNull();
+  });
+
+  it('retona undefined caso não seja passado nenhum argumento', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+
+  it('retorna uma mensagem ao passar um argumento que não é uma string', () => {
+    expect(handlerElephants(0)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+
+  it('retorna um valor do objeto caso o parâmetro seja uma key do objeto', () => {
+    expect(handlerElephants('location')).toBe('NW');
+    expect(handlerElephants('popularity')).toBe(5);
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+});
