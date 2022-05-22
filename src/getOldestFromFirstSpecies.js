4155701d32894d2ca8724ff7064d32a22b73ae5c
Henryck <ryck302@gmail.com>
2022-05-21 23:13:57 -0300

    requsito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const responsibleFor = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  const firstAnimal = data.species.find((specie) => specie.id === responsibleFor);
+  const oldestAnimal = firstAnimal.residents.reduce((oldest, animal) => {
+    if (oldest.age > animal.age) return oldest; return animal;
+  });
+  return Object.values(oldestAnimal);
 }
 
 module.exports = getOldestFromFirstSpecies;
