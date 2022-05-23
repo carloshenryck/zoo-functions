a6cc5212a7bee8492208d87fcda3f2dc5da92e3f
Henryck <ryck302@gmail.com>
2022-05-23 01:18:43 -0300

    requsito 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,63 +1,54 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const locations = [...new Set(data.species.map((specie) => specie.location))];
-
-function getAnimalsByName(name) {
+function animalObjectNotFiltered(specie) {
   return {
-    [name]: data.species.find((specie) => specie.name === name).residents
-      .map((resident) => resident.name),
+    [specie.name]: specie.residents.map((resident) => resident.name),
   };
 }
 
-function getAnimalsLocation() {
-  const map = { };
-
-  locations.forEach((local) => {
-    map[local] = data.species.filter((specie) => specie.location === local)
-      .map((animal) => animal.name);
+function mapNotFiltered(map) {
+  species.forEach((specie) => {
+    map[specie.location].push(animalObjectNotFiltered(specie));
   });
   return map;
 }
 
-function getLocationWithNames() {
-  const map = getAnimalsLocation();
-
-  Object.entries(map).forEach((entrie) => {
-    const location = entrie[0];
-    const animals = entrie[1];
-    const animalsWithName = animals.map((animal) => getAnimalsByName(animal));
-
-    map[location] = animalsWithName;
-  });
-
-  return map;
-}
-
-function getLocationFilteredBySex(sex, animalsObject) {
-  Object.values(animalsObject).forEach((animalArray) => {
-    animalArray.forEach((animal) => {
-      const specie = Object.keys(animal)[0];
-      const names = Object.values(animal)[0];
-      const filtered = names.filter((name) => data.species.find((oxi) => oxi.name === specie)
-        .residents.find((resident) => resident.name === name && resident.sex === sex));
-      animal[specie] = filtered;
-    });
-  });
-}
-
-// eslint-disable-next-line complexity
-function getAnimalMap(options) {
-  if (!options) return getAnimalsLocation();
-  const keys = Object.keys(options);
-  if (!keys.includes('includeNames') && keys.length > 0) return getAnimalsLocation();
-  if (keys.includes('includeNames') && keys.length === 1) return getLocationWithNames();
-  let animalMap;
-  if (keys.includes('sex')) {
-    animalMap = getLocationWithNames();
-    getLocationFilteredBySex(options.sex, animalMap);
+function animalObjectFiltered(specie, options) {
+  if (options.sex) {
+    const animalObject = { [specie.name]: specie.residents.filter((resident) =>
+      resident.sex === options.sex).map((animal) => animal.name) };
+    if (options.sorted) {
+      animalObject[specie.name].sort();
+    }
+    return animalObject;
   }
 
-  return animalMap;
+  if (options.sorted) {
+    return { [specie.name]: specie.residents.map((resident) => resident.name).sort() };
+  }
+}
+
+function mapFiltered(map, options) {
+  species.forEach((specie) => {
+    map[specie.location].push(animalObjectFiltered(specie, options));
+  });
+  return map;
+}
+
+function getAnimalMap(options) {
+  const map = { NE: [], NW: [], SE: [], SW: [] };
+
+  if (!options || !options.includeNames) {
+    data.species.forEach((specie) => map[specie.location].push(specie.name));
+    return map;
+  }
+
+  if (Object.keys(options).length === 1) {
+    return mapNotFiltered(map);
+  }
+
+  return mapFiltered(map, options);
 }
 
 module.exports = getAnimalMap;
